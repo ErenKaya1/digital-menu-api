5ef3f0dabc2cc83cdd7d96104ed848dffd853d12
ErenKaya1 <wtcerberus@gmail.com>
2021-03-16 16:17:45 +0300

    added Encrypted attribute for properties to be encrypted

---
+++ src/DigitalMenu.Core/Attribute/EncryptedAttribute.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace DigitalMenu.Core.Attribute
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
+    public class EncryptedAttribute : System.Attribute
+    {
+
+    }
+}
--- src/DigitalMenu.Entity/Entities/DMUser.cs
+++ src/DigitalMenu.Entity/Entities/DMUser.cs
@@ -1,6 +1,7 @@
 using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using DigitalMenu.Core.Attribute;
 using DigitalMenu.Entity.Entities.Base;
 
 namespace DigitalMenu.Entity.Entities
@@ -21,12 +22,14 @@ namespace DigitalMenu.Entity.Entities
         public string LastName { get; set; }
 
         [Required]
+        [Encrypted]
         public string EmailAddress { get; set; }
 
         [Required]
         public string PasswordHash { get; set; }
 
         [Required]
+        [Encrypted]
         public string PhoneNumber { get; set; }
 
         [Required]

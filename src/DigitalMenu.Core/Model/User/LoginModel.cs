70c29556977576f76b9d7ca1418d6e28a2a040ba
ErenKaya1 <wtcerberus@gmail.com>
2021-03-17 03:29:37 +0300

    added LoginModel.cs

---
+++ src/DigitalMenu.Core/Model/User/LoginModel.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace DigitalMenu.Core.Model.User
+{
+    public class LoginModel
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public bool IsPersistent { get; set; }
+    }
+}
